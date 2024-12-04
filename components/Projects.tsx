ab8ebea8f8d377f8b545bde10b3c571a5fc1b28a
Keyur <43619476+keyur104@users.noreply.github.com>
2024-12-04 16:31:55 -0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -65,7 +65,7 @@ export default function Projects() {
         </motion.div>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div
               key={project.title}
               initial={{ opacity: 0, y: 20 }}
